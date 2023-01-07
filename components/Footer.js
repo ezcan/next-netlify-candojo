bf5cd998b54c29cac3b6cec7778448914a693877
ezcan <kuanjunglien@gmail.com>
2023-01-07 21:55:32 +0800

    add markdown

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ export default function Footer() {
   return (
     <footer className='bg-fuchsia-100 mt-8 py-4'>
         <div className='container mx-auto flex justify-center'>
-          &copy; 2023 CanDoJo
+          &copy; 2023 ezcan
         </div>
       </footer>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,53 @@
 import Head from 'next/head'
+import fs from 'fs';
+import matter from 'gray-matter';
+import Image from 'next/image';
+import Link from 'next/link';
+
+export async function getStaticProps() {
+  // Get all our posts
+  const files = fs.readdirSync('posts');
+  
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const readFile = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const { data: frontmatter } = matter(readFile);
+    
+      return {
+        slug,
+        frontmatter,
+      };
+    });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+}
 
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
-    (<div>Hello world 👋</div>)
-  )
+    <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 p-4 md:p-0'>
+      {posts.map(({ slug, frontmatter }) => (
+        <div
+          key={slug}
+          className='border border-gray-200 m-2 rounded-xl shadow-lg overflow-hidden flex flex-col'
+        >
+          <Link href={`/post/${slug}`}>
+            <a>
+              <Image
+                width={650}
+                height={340}
+                alt={frontmatter.title}
+                src={`/${frontmatter.socialImage}`}
+              />
+              <h1 className='p-4'>{frontmatter.title}</h1>
+            </a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
